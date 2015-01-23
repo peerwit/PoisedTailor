22817a41cdea1d163d1f8eb82cfb6ccb175a5467
peerwit <pio@PIOs-MacBook-Air.local>
2015-01-23 13:40:27 -0800

    Board isValid DONE

--- client/assets/gameLogic/Board.js
+++ client/assets/gameLogic/Board.js
@@ -4,13 +4,25 @@
 var Board = function() {
 	this.state = [[]];
 }
+	
+Board.prototype.operationsList = Board.prototype.opsList = {
+	add:function (a, b){return a + b},
+	addition:function (a, b){return a + b},
+	sub:function (a, b){return a - b},
+	subtraction:function (a, b){return a - b},
+	multiplication:function (a, b) {return a * b},
+	div:function (a, b) {return a / b},
+	division:function (a, b) {return a / b},
+	modulo:function (a, b) {return a % b},
+	mod:function (a, b) {return a % b}
+}
 
 Board.prototype.create = function(m,n,operation, difficulty) {
 	this.rows = m || 12;
 	this.cols = n || 6;
-	this.op = this.operation = operation || "+";
+	this.op = this.operation = operation || this.opsList.add;
 	this.diff = this.difficulty = difficluty || 1;
-	this.state = this.makeBoard(this.rows, this.cols);
+	this.state = this.board = this.makeBoard(this.rows, this.cols);
 	return this.state;
 }
 
@@ -35,7 +47,7 @@ Board.prototype.makeBoard = function(m,n) {
 
 Board.prototype.setBoardInit = Board.prototype.init =  function(min, max){
 	this.range = [(min||0), (max||9)];
-	this.sum = this.range[0] + this.range[1];
+	this.target = this.range[0] + this.range[1];
 	var board = this.state;
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board[i].length; j++) {
@@ -44,33 +56,55 @@ Board.prototype.setBoardInit = Board.prototype.init =  function(min, max){
 	}
 }
 
+Board.prototype.getValueAtTuple = Board.prototype.getVal =  function(tuple){
+	return this.board[tuple[0]][tuple[1]];
+}
+
 Board.prototype.generateRandom = Board.prototype.ran = function() {
 	var range = this.range;
-	var sum = this.sum;
+	var target = this.target;
 	return Math.floor(Math.random()*(this.range[1] - this.range[0])) + this.range[0];
 }
 
-Board.prototype.isInBounds = function(t1, t2) {
-	if (t1[0] > board.length - 1 || t1[0] < 0 || t1[1] > board[0].length - 1 || t1[1] < 0 ||
-		t2[0] > board.length - 1 || t2[0] < 0 || t2[1] > board[0].length - 1 || t2[1] < 0) {
+Board.prototype.isInBounds = function(tuple, board) {
+	board = board || this.board;
+	if (tuple[0] > board.length - 1 || tuple[0] < 0 || tuple[1] > board[0].length - 1 || tuple[1] < 0) {
 		return false;
 	}
 	return true;
 } 
 
 // Pass in two arrays t1 = [i1, j1] &&  t2 = [i2, j2]
-Board.prototype.isValid = function(t1, t2){
+Board.prototype.isValidSwap = Board.prototype.isValid = function(t1, t2){
 	// are t1 && t2 neighbors ? continue; break;
-	if (!this.isInBounds(t1,t2)) {
+	if (!this.isInBounds(t1) || !this.isInBounds(t2)) {
 		return false;
 	}
 	else {
-		
+		return this.operatesToTarget(t1) || this.operatesToTarget(t2);
 	}
 }
 
-Board.prototype.isConnected = function(){
-	return this.BFS().length === this.al.length
+Board.prototype.operatesToTarget = Board.prototype.operates = function(tuple, target, operation, board){
+	target = target || this.target;
+	operation = operation || this.operation;
+	board = board || this.board;
+
+	var neighbors = [];
+	neighbors.push([tuple[0] + 1, tuple[1]]);
+	neighbors.push([tuple[0] - 1, tuple[1]]);
+	neighbors.push([tuple[0], tuple[1] - 1]);
+	neighbors.push([tuple[0], tuple[1] + 1]);
+
+	neighbors = neighbors.filter(function(e) {
+		return this.isInBounds(e);
+	});
+
+	return neighbors.some(function(e) {
+		var a = this.getVal(e);
+		var b = this.getVal(tuple);
+		return this.op(a,b) === target;
+	})
 }
 
 Board.prototype.d3ify = function(){
