0c51e5e133c5271ea16c185054c500b6b650f36a
peerwit <pio@PIOs-MacBook-Air.local>
2015-01-30 17:30:24 -0800

    fixed refresh

--- client/assets/gameLogic/Board.js
+++ client/assets/gameLogic/Board.js
@@ -1,11 +1,15 @@
-// Simple Boards implementation by Pranay and Alex Tseung
-	// v 0.0.1 (01/23/15)
+// Simple Boards implementation by Pranay
+	// v 0.0.1 (01/29/15)
 
-// BOARD CLASS - gives you a new playable board
+// BOARD CLASS - gives you a new playable board DEPENDS on Graphyc.js
 var Board = function(m, n, operation, target, difficulty) {
 	this.state = this.board = [[]];
 	this._create(m, n, operation, target, difficulty);
+	this.score = 0;
+	this.__render = false;
+	this.__map = {board: 'state', state: 'board'};
 }
+
 
 // Takes a string or an array and gets board attributes
 // WILL mutate array
@@ -24,6 +28,13 @@ Board.prototype.get = function(attr) {
 	}
 }
 
+Board.prototype.set = function(attr, val) {
+	if (attr in this.__map) {
+		this[this.__map[attr]] = val
+	}
+	this[attr] = val;
+}
+
 	
 Board.prototype.operationsList = Board.prototype.opsList = {
 	add:function (a, b){return a + b},
@@ -37,12 +48,15 @@ Board.prototype.operationsList = Board.prototype.opsList = {
 	mod:function (a, b) {return a % b}
 }
 
-Board.prototype._create = function(m, n, operation, difficulty) {
+Board.prototype._create = function(m, n, operation, target, difficulty) {
 	this.rows = m || this.rows || 12;
 	this.cols = n || this.cols || 6;
+	this.target = this.target || target || 9;
 	this.op = this.operation = operation || this.op || this.opsList.add;
+	this.opName = operation || "addition";
 	this.diff = this.difficulty = difficulty || this.diff || 1;
 	this.state = this.board = this._makeBoard(this.rows, this.cols);
+	this._isPlayable()?null:this._refresh();
 	return this;
 }
 
@@ -68,11 +82,36 @@ Board.prototype.swap = Board.prototype.makeSwap = function(t1, t2, cb, cb2){
 
 	// callback for interacting with the nodes which operate to the target
 	cb(array);
-
-
 	this._regenConsumedNodes(array);
 	this._isPlayable()?null:this._refresh();
+	this.score += (1000 * array.length);
+	return this;
+}
 
+Board.prototype.swapForce = function(t1, t2, cb, cb2){
+	if (!t1 || !t2){throw new Error ('cannot call swap without two tuples')}
+	if (typeof t1 !== 'object' || typeof t2 !== 'object'){throw new Error ('cannot call swap without two tuples')}
+	if (!t1.length || !t2.length){throw new Error ('cannot call swap without two tuples')}
+	if (t1.length !== 2 || t2.length !== 2){throw new Error ('cannot call swap without two tuples')}
+
+	if (!this.isValidSwap(t1,t2)) {
+		throw new Error ('has inValid swap. Call the isValidSwap fn before calling swap.')
+	}
+
+	cb = cb || function(){};
+	cb2 = cb2 || function(){};
+
+	var array = this.idMatches(t1, t2).concat(this.idMatches(t2,t1));
+	// console.log("FDs", array, this.idMatches(t1, t2), this.idMatches(t1, t2));
+	// swaps the tuples on the board
+	var temp = this._get(t1);
+	this._set(t1, this._get(t2));
+	this._set(t2, temp);
+
+	// callback for interacting with the nodes which operate to the target
+	cb(array);
+	this._regenConsumedNodes(array);
+	this.score += (1000 * array.length);
 	return this;
 }
 
@@ -90,8 +129,9 @@ Board.prototype.generateRandom = Board.prototype.ran = function() {
 }
 
 Board.prototype.isInBounds = function(tuple, board) {
-	board = board || this.board;
-	if (tuple[0] > board.length - 1 || tuple[0] < 0 || tuple[1] > board[0].length - 1 || tuple[1] < 0) {
+	board = this.board;
+	// console.log(board.length - 1, board[0].length - 1);
+	if (+tuple[0] > (board.length - 1) || +tuple[0] < 0 || +tuple[1] > (board[0].length - 1) || +tuple[1] < 0) {
 		return false;
 	}
 	return true;
@@ -102,13 +142,13 @@ Board.prototype.isValidSwap = Board.prototype.isValid = function(t1, t2){
 	if (!this.isInBounds(t1) || !this.isInBounds(t2)) {
 		return false;
 	}
-	// console.log(this._getNeighbors(t2), t1)
 
-	// if (this._getNeighbors(t2).indexOf(t1) === -1) {
-	// 	return false;
-	// }
+	if (this._getNeighbors(t2).map(function(e) {return JSON.stringify(e)}).indexOf(JSON.stringify(t1)) === -1) {
+		console.log("got one!")
+		return false;
+	}
 	// console.log(t1,t2);
-	return this.operatesToTarget(t1, t2);
+	return this.operatesToTarget(t1, t2) || this.operatesToTarget(t2, t1);
 }
 
 Board.prototype.operatesToTarget = Board.prototype.operates = function(t1, t2, target, operation, board){
@@ -131,6 +171,19 @@ Board.prototype.operatesToTarget = Board.prototype.operates = function(t1, t2, t
 	return false;
 }
 
+Board.prototype.autoSwapper = function(cb){
+	target = target || this.target;
+	operation = operation || this.operation;
+	board = this.board;
+	var that = this;
+	this._iterate(function(tuple) {
+		var n = that._getNeighbors(tuple);
+		for (var i = 0; i < n.length; i++) {
+			that.isValidSwap(tuple, n[i]) ? cb(tuple, n[i]) : null;
+		}
+	});
+
+}
 
 Board.prototype.idMatches = function(currentTuple, proposedTuple, target, operation, board) {
 	target = target || this.target;
@@ -172,8 +225,7 @@ Board.prototype._makeBoard = function(m,n) {
 	this.state = this.board = board;
 	this._init();
 	this._removeMatches();
-	this._isPlayable()?null:this._refresh();
-	return board;
+	return this.board;
 }
 
 Board.prototype._regenConsumedNodes = function(array) {
@@ -185,7 +237,7 @@ Board.prototype._regenConsumedNodes = function(array) {
 	array.forEach(function(e) {
 		that._updateIfMatch(e);
 	})
-	return board;
+	return this;
 }
 
 Board.prototype._isPlayable = function() {
@@ -200,10 +252,14 @@ Board.prototype._isPlayable = function() {
 		var neighbors = this._getNeighbors(tuple);
 		var that = this;
 		var val = board[tuple[0]][tuple[1]];
+		// console.log(tuple, val, board);
 		var flag = neighbors.some(function(e) {
 			var n2s = that._getNeighbors(e);
 			return n2s.some(function(n2e) {
-				return op(val, board[n2e[0]][n2e[1]]) === target;
+				if (JSON.stringify(tuple) !== JSON.stringify(n2e)) {
+					// console.log(op(val, board[n2e[0]][n2e[1]]) === target ? n2e+"p"+tuple : null);
+					return op(val, board[n2e[0]][n2e[1]]) === target;
+				}
 			})
 		})
 		if (flag) {
@@ -214,13 +270,18 @@ Board.prototype._isPlayable = function() {
 }
 
 Board.prototype._refresh = function() {
-	console.log(this.range, this.rows, this.cols);
-	this._create(this.range[0],this.range[1]);
+	// console.log('refreshing', this.rows, this.cols)
+	this._create(this.rows,this.cols);
+	while(!this._isPlayable()){this._makeBoard(this.rows, this.cols)}
+	// console.log(this.board);
+	if (this.__render) {
+		this._render();
+	}
 }
 
 Board.prototype._setBoardInit = Board.prototype._init =  function(min, max){
 	this.range = [(min||0), (max||9)];
-	this.target = this.range[0] + this.range[1];
+	// this.target = this.range[0] + this.range[1];
 	var board = this.state;
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board[i].length; j++) {
@@ -315,7 +376,7 @@ Board.prototype._updateIfMatch = function(tuple){
 		neighborValHash[board[n[0]][n[1]]] = board[n[0]][n[1]];
 	})
 	flag ? (board[tuple[0]][tuple[1]] = update(board[tuple[0]][tuple[1]], that.op, that.target)) : null;
-	
+	this.board = board;
 
 
 	function update(val, fn, target) {
@@ -343,6 +404,10 @@ Board.prototype._removeMatches = function() {
 	this._iterate(this._updateIfMatch.bind(this));
 }
 
+Board.prototype.genId = function(tuple) {
+	//
+}
+
 // var readline = require('readline');
 
 // var rl = readline.createInterface({
@@ -355,16 +420,19 @@ Board.prototype._removeMatches = function() {
 // 	var t1 = JSON.parse(a.shift());
 // 	var t2 = JSON.parse(a.shift());
 // 	console.log("IS VALID", b1.isValidSwap(t1,t2));
+// 	console.log(b1.isValidSwap(t1,t2)?null:"CANNOT SWAP");
 // 	b1.swap(t1,t2);
 // 	console.log(b1.get(['state', 'target']));
-// 	rl.close();
+// 	// rl.close();
 // });
 
-// // ----
-// var b1 = new Board();
+// ----
+var b1 = new Board(2, 4);
+b1.set('state', [[1,0,1,0],[0,0,0,0]]);
+b1.set('target', 2);
+b1.swap([0,0],[0,1])
+console.log(b1.get('state'), "+++", b1.get('target'));
 
-// console.log(b1.get('state'), "+++");
-// b1.swap([0,0],[0,1])
 
 
 
@@ -382,13 +450,12 @@ Board.prototype._removeMatches = function() {
 
 
 
-var testboard = new Board();
-testboard.board = [[1,0,1,0],[0,0,0,0]];
-console.log(testboard.board)
-testboard.target = 2;
-testboard.swap([0,0],[0,1]);
-console.log(testboard.board);
-
+// var testboard = new Board();
+// testboard.board = [[1,0,1,0],[0,0,0,0]];
+// console.log(testboard.board)
+// testboard.target = 2;
+// testboard.swap([0,0],[0,1]);
+// console.log(testboard.board);
 
 
 
